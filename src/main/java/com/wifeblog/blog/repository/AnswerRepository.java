3c6101b2e11ce64f453c3c7d32532aa6e7e0347c
Matthew <matthew.d.baker20@gmail.com>
2021-04-03 15:20:28 -0500

    comments can be approved by admin

---
+++ src/main/java/com/wifeblog/blog/repository/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.wifeblog.blog.repository;
+
+import com.wifeblog.blog.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+}
